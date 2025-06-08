779b3f44d524aebe4a53545e87d396fab964e47d
Cristian Roca <cristian.roc30@gmail.com>
2025-06-08 18:00:13 +0200

    query 1

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,31 @@
-import postgres from 'postgres';
+import postgres from 'postgres'; 
+
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
 async function listInvoices() {
-	const data = await sql`
+  const data = await sql`
     SELECT invoices.amount, customers.name
     FROM invoices
     JOIN customers ON invoices.customer_id = customers.id
     WHERE invoices.amount = 666;
   `;
 
-	return data;
+  return data;
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
   try {
-  	return Response.json(await listInvoices());
+    return Response.json(await listInvoices());
   } catch (error) {
-  	return Response.json({ error }, { status: 500 });
+    console.error("Error al consultar la base de datos:", error);
+    let errorMessage = 'Ocurrió un error desconocido';
+    if (error instanceof Error) {
+      errorMessage = error.message; 
+    } else if (typeof error === 'string') {
+      errorMessage = error; 
+    }
+    return Response.json({ error: errorMessage }, { status: 500 });
   }
 }
