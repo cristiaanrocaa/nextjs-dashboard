898dcc7a09f5c09df5e18a000c44e5d905976f9f
Cristian Roca <cristian.roc30@gmail.com>
2025-06-08 17:46:25 +0200

    a

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,11 +1,9 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcryptjs'; // Usa 'bcryptjs' si 'bcrypt' te da problemas
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-async function seedUsers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedUsers(sql: postgres.Sql<{}>) { 
   await sql`
     CREATE TABLE IF NOT EXISTS users (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -29,9 +27,7 @@ async function seedUsers() {
   return insertedUsers;
 }
 
-async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
+async function seedInvoices(sql: postgres.Sql<{}>) { 
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -55,9 +51,7 @@ async function seedInvoices() {
   return insertedInvoices;
 }
 
-async function seedCustomers() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-
+async function seedCustomers(sql: postgres.Sql<{}>) { 
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
@@ -80,7 +74,7 @@ async function seedCustomers() {
   return insertedCustomers;
 }
 
-async function seedRevenue() {
+async function seedRevenue(sql: postgres.Sql<{}>) { 
   await sql`
     CREATE TABLE IF NOT EXISTS revenue (
       month VARCHAR(4) NOT NULL UNIQUE,
@@ -103,15 +97,29 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
-    const result = await sql.begin((sql) => [
-      seedUsers(),
-      seedCustomers(),
-      seedInvoices(),
-      seedRevenue(),
-    ]);
+
+    const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+    await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+
+    await sql.begin(async (t) => {
+      await seedUsers(t);
+      await seedCustomers(t);
+      await seedInvoices(t);
+      await seedRevenue(t);
+    });
+
+    await sql.end(); // Cerrar la conexión después de la transacción
 
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    console.error("Error seeding database:", error); 
+    let errorMessage = 'An unknown error occurred';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else if (typeof error === 'string') {
+      errorMessage = error;
+    }
+    return Response.json({ error: errorMessage }, { status: 500 });
   }
 }
